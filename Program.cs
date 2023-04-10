167e98252046a9e5320759261147816f2541e92e
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-04-10 10:34:12 -0300

    exercicio executado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int x = 1;
+int y = 1;
+
+do
+{
+   Console.WriteLine($"{x} AINDA MENOR QUE 500");
+   Console.WriteLine($"{y} AINDA MENOR QUE 500");
+   x = x+y;
+   y = y+x;
+    
+} while (x < 500 );
+
+Console.WriteLine($"{x}");
+Console.WriteLine($"{y}");
